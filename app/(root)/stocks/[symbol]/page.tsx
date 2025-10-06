f24f57b5397d1e96f6549e2ac9050ff2ed9b0586
sigmadev92 <devanshofficial2000@gmail.com>
2025-10-06 16:53:37 +0530

    Implemented the stock detail page

---
+++ app/(root)/stocks/[symbol]/page.tsx
@@ -0,0 +1,66 @@
+import TradingViewWidget from "@/components/TradingViewWidget";
+import WatchlistButton from "@/components/WatchlistButton";
+import {
+  SYMBOL_INFO_WIDGET_CONFIG,
+  CANDLE_CHART_WIDGET_CONFIG,
+  BASELINE_WIDGET_CONFIG,
+  TECHNICAL_ANALYSIS_WIDGET_CONFIG,
+  COMPANY_FINANCIALS_WIDGET_CONFIG,
+} from "@/lib/constants";
+
+export default async function StockDetails({ params }: StockDetailsPageProps) {
+  const { symbol } = await params;
+  const scriptUrl = `https://s3.tradingview.com/external-embedding/embed-widget-`;
+
+  return (
+    <div className="flex min-h-screen p-4 md:p-6 lg:p-8">
+      <section className="grid grid-cols-1 md:grid-cols-2 gap-8 w-full">
+        {/* Left column */}
+        <div className="flex flex-col gap-6">
+          <TradingViewWidget
+            scriptURL={`${scriptUrl}symbol-info.js`}
+            config={SYMBOL_INFO_WIDGET_CONFIG(symbol)}
+            height={170}
+          />
+
+          <TradingViewWidget
+            scriptURL={`${scriptUrl}advanced-chart.js`}
+            config={CANDLE_CHART_WIDGET_CONFIG(symbol)}
+            className="custom-chart"
+            height={600}
+          />
+
+          <TradingViewWidget
+            scriptURL={`${scriptUrl}advanced-chart.js`}
+            config={BASELINE_WIDGET_CONFIG(symbol)}
+            className="custom-chart"
+            height={600}
+          />
+        </div>
+
+        {/* Right column */}
+        <div className="flex flex-col gap-6">
+          <div className="flex items-center justify-between">
+            <WatchlistButton
+              symbol={symbol.toUpperCase()}
+              company={symbol.toUpperCase()}
+              isInWatchlist={false}
+            />
+          </div>
+
+          <TradingViewWidget
+            scriptURL={`${scriptUrl}technical-analysis.js`}
+            config={TECHNICAL_ANALYSIS_WIDGET_CONFIG(symbol)}
+            height={400}
+          />
+
+          <TradingViewWidget
+            scriptURL={`${scriptUrl}financials.js`}
+            config={COMPANY_FINANCIALS_WIDGET_CONFIG(symbol)}
+            height={464}
+          />
+        </div>
+      </section>
+    </div>
+  );
+}
--- components/Header.tsx
+++ components/Header.tsx
@@ -3,8 +3,10 @@ import Link from "next/link";
 import React from "react";
 import NavItems from "./NavItems";
 import UserDropdown from "./UserDropdown";
+import { searchStocks } from "@/lib/actions/finnhub.actions";
 
-const Header = ({ user }: { user: User }) => {
+const Header = async ({ user }: { user: User }) => {
+  const initialStocks = await searchStocks();
   return (
     <header className="sticky top-0">
       <div className="container header-wrapper">
@@ -18,10 +20,10 @@ const Header = ({ user }: { user: User }) => {
           />
         </Link>
         <nav className="hidden sm:block">
-          <NavItems />
+          <NavItems initialStocks={initialStocks} />
         </nav>
         {/* User component */}
-        <UserDropdown user={user} />
+        <UserDropdown user={user} initialStocks={initialStocks} />
       </div>
     </header>
   );
